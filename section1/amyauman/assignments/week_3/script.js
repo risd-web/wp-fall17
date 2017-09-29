5ed9a69b894712c2a9dffeaed6fe8d9a7d3b3d7b
acauman <aauman@risd.edu>
2017-09-29 10:55:25 -0400

    working with animation

---
+++ section1/amyauman/assignments/week_3/script.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function(){
+
+		console.log("check for animation click");
+
+	$('body').click(function(){
+
+		$('.circle').toggleClass("animate");
+		$('.smalltriangle').toggleClass("animate");
+
+	});
+
+	});
