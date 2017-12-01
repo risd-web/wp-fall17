253422f92f943508731ccd2b7608402ed13ef84f
Cynthia <clee18@risd.edu>
2017-12-01 02:22:01 -0500

    update

--- section3/cynthia-lee/HOMEWORK/jquery/script.js
+++ section3/cynthia-lee/HOMEWORK/jquery/script.js
@@ -7,10 +7,35 @@ $(document).ready(function(){
 	});
 
 
-	$(".element").click(function(){
-		console.log("clicking element");
+	$(".box").click(function(){
+		console.log("clicking box");
 		$(this).toggleClass('play');
 	});
+
+
+	$(".click2").click(function(){
+		console.log("check for button click");
+		$("img").toggleClass('reveal');
+	});
+
+
+	$(".box2").click(function(){
+		console.log("clicking box");
+		$(this).toggleClass('play');
+	});
+
+$(".click3").click(function(){
+		console.log("check for button click");
+		$("h3").toggleClass('reveal');
+	});
+
+
+	$(".box3").click(function(){
+		console.log("clicking box");
+		$(this).toggleClass('play');
+	});
+
+
 
 
 });
