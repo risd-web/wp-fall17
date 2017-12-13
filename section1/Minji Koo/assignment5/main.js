5503338d3c3a8f71e97ef5180ffac19de106dc97
mkoo01 <31776285+mkoo01@users.noreply.github.com>
2017-12-13 17:46:25 -0500

    jkl
    fghj

--- section1/Minji Koo/assignment5/main.js
+++ section1/Minji Koo/assignment5/main.js
@@ -1,2 +1,8 @@
-var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Whenever I have a day off, I write a new poem.';
+$(function() {
+	$("#button").click(function() {
+		$('.rabbit').toggleClass('big');
+	});
+});
+
+//var myHeading = document.querySelector('h1');
+//myHeading.textContent = 'Whenever I have a day off, I write a new poem.';
