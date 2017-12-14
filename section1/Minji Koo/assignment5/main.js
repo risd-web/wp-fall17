42c4657264a72628b0add92859f71136af551ae4
tomirissh <tomirissh@users.noreply.github.com>
2017-12-14 16:20:20 -0500

    upload

--- section1/Minji Koo/assignment5/main.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-	$("#button").click(function() {
-		$('.rabbit').toggleClass('big');
-	});
-});
-
-//var myHeading = document.querySelector('h1');
-//myHeading.textContent = 'Whenever I have a day off, I write a new poem.';
