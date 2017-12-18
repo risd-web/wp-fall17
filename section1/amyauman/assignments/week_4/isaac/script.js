3f6c776390494ac9029c6c620bb3cf792d877c55
Marie Otsuka <marieotsuka@users.noreply.github.com>
2017-12-18 03:33:38 -0500

    Revert "upload"
    This reverts commit 42c4657264a72628b0add92859f71136af551ae4.

---
+++ section1/amyauman/assignments/week_4/isaac/script.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function(){
+
+		console.log("check for animation click");
+
+	$('h2').click(function(){
+
+		$('h2').toggleClass("animate");
+
+	});
+
+		$('h1').click(function(){
+
+		$('h1').toggleClass("animate2");
+	});
+
+	});
+
+
